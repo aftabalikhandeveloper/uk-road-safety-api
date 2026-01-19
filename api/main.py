a19194c5b70fd06c9c15712ee9766f154d514534
aftabalikhandeveloper <aftabalikhan900@gmail.com>
2026-01-19 11:00:22 +0500

    updated requirments.txt with missing modules

--- api/main.py
+++ api/main.py
@@ -168,6 +168,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.main:app",
         host="0.0.0.0",
-        port=8000,
+        # from environment variable or default to 8080
+        port=int(os.getenv("API_PORT", 8080)),
         reload=True
     )
